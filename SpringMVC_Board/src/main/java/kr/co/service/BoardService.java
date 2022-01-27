2401235ddb4d2c9d68312359d0bea5a88811e353
kkwkkj@naver.com <kkwkkj@naver.com>
2022-01-27 20:03:26 +0900

    수정

--- SpringMVC_Board/src/main/java/kr/co/service/BoardService.java
+++ SpringMVC_Board/src/main/java/kr/co/service/BoardService.java
@@ -11,9 +11,9 @@ public interface BoardService {
 	//작성
 	public void write (BoardVO boardVO) throws Exception;
 	
-	//리스트
-//	public List<BoardVO> list() throws Exception;
 	/**
+	//리스트
+	public List<BoardVO> list() throws Exception;
 	public List<BoardVO> list(Criteria cri) throws Exception;
 	public int listCount() throws Exception;
 	*/
